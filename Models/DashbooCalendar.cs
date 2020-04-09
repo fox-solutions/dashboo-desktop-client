3ebdb1be7d2b7125466d74ddf3012f06ee0afad7
Krisuuuuuu <krisu1997@onet.pl>
2020-04-09 21:46:10 +0200

    Added class of Calendar into Models and fixed bug related to displaying month name on the user control

---
+++ Models/DashbooCalendar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashbooCalendar.Models
+{
+    class DashbooCalendar
+    {
+        public DateTime Today { get; private set; }
+
+        public string[] ShortDayNames { get; set; }
+
+        private string monthName;
+
+        public string MonthName
+        {
+            get
+            {
+                return monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(Today.Month);
+            }
+        }
+
+        public IEnumerable<DashbooTile> DashbooTiles { get; private set; }
+
+        private int dashbooTilesCounter;
+
+        public int DashbooTilesCounter
+        {
+            get
+            {
+                return dashbooTilesCounter = DashbooTiles.Count();
+            }
+        }
+
+        public DashbooCalendar()
+        {
+            Today = DateTime.Now;
+            DashbooTiles = new List<DashbooTile>();
+            ShortDayNames = new string[7];
+
+            for(int i=0; i<7; i++)
+            {
+                ShortDayNames[i] = ((DayOfWeek)i).ToString();
+            }
+        }
+    }
+}
