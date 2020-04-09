9fd4377c0f9305c74903a19f2cdc423cbf1dc4d6
Krisuuuuuu <krisu1997@onet.pl>
2020-04-09 20:54:12 +0200

    Added classes of Events and Tiles into Models

---
+++ Models/DashbooEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashbooCalendar.Models
+{
+    public class DashbooEvent
+    {
+        public int DashbooEventId { get; private set; }
+
+        public string Title { get; private set; }
+
+        public string Description { get; private set; }
+
+        public DateTime EventDate { get; private set; }
+
+        public DashbooEvent(string title, string description, DateTime eventDate)
+        {
+            Title = title;
+            Description = description;
+            EventDate = eventDate;
+        }
+    }
+}
---
+++ Models/DashbooTile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashbooCalendar.Models
+{
+    public class DashbooTile
+    {
+        public int TileId { get; private set; }
+
+        public DateTime Date { get; private set; }
+
+        private string fullDayName;
+
+        public string FullDayName
+        {
+            get
+            {
+               return fullDayName = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedDayName(Date.DayOfWeek);
+            }
+        }
+
+        private string shortDayName;
+
+        public string ShortDayName
+        {
+            get
+            {
+                return shortDayName = CultureInfo.CurrentCulture.DateTimeFormat.GetShortestDayName(Date.DayOfWeek);
+            }
+        }
+
+        private string monthName;
+
+        public string MonthName
+        {
+            get
+            {
+                return monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(Date.Month);
+            }
+        }
+
+        public IEnumerable<DashbooEvent> dashbooEvents { get; private set; }
+
+        private int dashbooEventCounter;
+        public int DashbooEventCounter
+        {
+            get
+            {
+                return dashbooEventCounter = dashbooEvents.Count();
+            }
+        }
+
+        public DashbooTile(DateTime date)
+        {
+            Date = date;
+            dashbooEvents = new List<DashbooEvent>();
+        }
+
+    }
+}
