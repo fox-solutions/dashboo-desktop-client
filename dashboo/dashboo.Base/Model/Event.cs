344ff56dbdbbbd28d867209e9c14e01419aa6261
Concierge2k <codeconcierge2k@gmail.com>
2020-04-15 14:34:35 +0200

    Create Tile and Event models for dashboo.Calendar

---
+++ dashboo/dashboo.Base/Model/Event.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace dashboo.Base.Model
+{
+	public class Event
+    {
+		#region Constructor
+		public Event()
+		{
+			Id = 0;
+			Title = "Event";
+			Description = "Description";
+			Duration = new DateTime(0, 0, 0);
+		}
+
+		public Event(string title, string description)
+		{
+			Title = title;
+			Description = description;
+		}
+
+		#endregion
+
+		#region Member
+		private int _id;
+
+		public int Id
+		{
+			get { return _id; }
+			set { _id = value; }
+		}
+
+		private string _title;
+
+		public string Title
+		{
+			get { return _title; }
+			set { _title = value; }
+		}
+
+		private string _description;
+
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+
+		private DateTime _duration;
+
+		public DateTime Duration
+		{
+			get { return _duration; }
+			set { _duration = value; }
+		}
+		#endregion
+	}
+}
---
+++ dashboo/dashboo.Base/Model/Tile.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+
+namespace dashboo.Base.Model
+{
+    public class Tile
+    {
+		#region Constructor
+		public Tile()
+		{
+			Id = 0;
+			Date = new DateTime(0, 0, 0);
+			events = new ObservableCollection<Event>();
+		}
+
+		public Tile(DateTime date)
+		{
+			Date = date;
+			events = new ObservableCollection<Event>();
+		}
+		#endregion
+
+		#region Member
+		private int _id;
+
+		public int Id
+		{
+			get { return _id; }
+			set { _id = value; }
+		}
+
+		private DateTime _date;
+
+		public DateTime Date
+		{
+			get { return _date; }
+			set { _date = value; }
+		}
+
+		private string _fullDayName;
+
+		public string FullDayName
+		{
+			get { return _fullDayName; }
+			set { _fullDayName = getFullDayName(); }
+		}
+
+		private string _shortDayName;
+
+		public string ShortDayName
+		{
+			get { return _shortDayName; }
+			set { _shortDayName = getShortDayName(); }
+		}
+
+		private string _monthName;
+
+		public string MonthName
+		{
+			get { return _monthName; }
+			set { _monthName = getMonthName(); }
+		}
+
+		private int _eventCounter;
+
+		public int EventCounter
+		{
+			get { return _eventCounter; }
+			set { _eventCounter = events.Count; }
+		}
+
+		public ICollection<Event> events;
+		#endregion
+
+		#region Method
+		public string getFullDayName()
+		{
+			return CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedDayName(Date.DayOfWeek);
+		}
+
+		public string getShortDayName()
+		{
+			return CultureInfo.CurrentCulture.DateTimeFormat.GetShortestDayName(Date.DayOfWeek);
+		}
+
+		public string getMonthName()
+		{
+			return CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(Date.Month);
+		}
+		#endregion
+	}
+}
