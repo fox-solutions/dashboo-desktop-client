a961716554e48b3cbe100754468a8e003984722a
Krisuuuuuu <krisu1997@onet.pl>
2020-04-15 12:14:53 +0200

    Added Base ViewModel

---
+++ dashboo/dashboo.Base/ViewModel/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace dashboo.Base.ViewModel
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected abstract void LoadModels();
+
+        protected abstract void LoadCommands();
+    }
+}
