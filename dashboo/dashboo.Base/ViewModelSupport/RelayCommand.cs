d416cb15e39df02f746998f7e6aa05bd39ccb49f
Krisuuuuuu <krisu1997@onet.pl>
2020-04-15 13:12:57 +0200

    Added RelayCommand

---
+++ dashboo/dashboo.Base/ViewModelSupport/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace dashboo.Base.ViewModelSupport
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Predicate<object> canExecute;
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute == null)
+            {
+                return true;
+            }
+            else
+            {
+                return canExecute.Invoke(parameter);
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            execute.Invoke(parameter);
+        }
+    }
+}
