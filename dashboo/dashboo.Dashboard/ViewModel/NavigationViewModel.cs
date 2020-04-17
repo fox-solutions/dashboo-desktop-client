e54a3fed1175209463b6a5e03ad06928ecdde10f
Kamiiiil <58139271+Kamiiiil@users.noreply.github.com>
2020-04-17 19:05:29 +0200

    Create NavigationView(UserControl) and NavigationViewModel (#19)

---
+++ dashboo/dashboo.Dashboard/ViewModel/NavigationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using dashboo.Base.ViewModelSupport.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dashboo.Dashboard.ViewModel
+{
+    public class NavigationViewModel : BaseViewModel
+    {
+        protected override void LoadCommands()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void LoadModels()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
