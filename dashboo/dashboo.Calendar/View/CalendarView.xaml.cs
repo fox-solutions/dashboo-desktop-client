aad1647fbcad1af9e00358eb5d447234818fa8aa
Concierge2k <codeconcierge2k@gmail.com>
2020-04-20 09:46:21 +0200

    reated calendar view & calendarviewmodel

---
+++ dashboo/dashboo.Calendar/View/CalendarView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dashboo.Calendar.View
+{
+    public partial class CalendarView : UserControl
+    {
+        public CalendarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
