476f4c84296e171ba1c03021fb629e047e9b080b
Krisuuuuuu <krisu1997@onet.pl>
2020-04-25 19:06:53 +0200

    Added part of Day view

--- dashboo/dashboo.Calendar/ViewModel/CalendarViewModel.cs
+++ dashboo/dashboo.Calendar/ViewModel/CalendarViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using dashboo.Common.Model;
+using System;
 using System.Collections.ObjectModel;
 
 namespace dashboo.Calendar.ViewModel
 {
     class CalendarViewModel
     {
-        public ObservableCollection<Tile> Tiles;
+        public ObservableCollection<Tile> Tiles = new ObservableCollection<Tile>()
+        {
+            new Tile(DateTime.Now)
+        };
+
+        public CalendarViewModel()
+        {
+
+        }
     }
 }
