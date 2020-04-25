500af5b74a6e550a5f83e5b796e4c1da20c2754d
Krzysiek Korona <48776828+Krisuuuuuu@users.noreply.github.com>
2020-04-25 20:59:15 +0200

    Added EventTypes enum (#34)

---
+++ dashboo/dashboo.Base/Model/Enum/EventTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dashboo.Base.Model.Enum
+{
+    public enum EventTypes
+    {
+        entertainment,
+        learning,
+        work,
+        rest,
+        selfDevelopment,
+        socialLife,
+        family
+    }
+}
